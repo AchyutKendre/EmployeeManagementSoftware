892817791f939fdfee2f9626dd4e1b8effffb2b2
Achyut Kendre <achyut.kendre@gmail.com>
2023-03-26 11:28:50 +0530

    Test Controller Added

---
+++ EmployeeManagementSoftware/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSoftware.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
