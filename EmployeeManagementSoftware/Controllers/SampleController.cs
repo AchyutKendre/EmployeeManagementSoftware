2d3b0c20065b898e40e6e9ae9390c19a11088e91
Achyut Kendre <achyut.kendre@gmail.com>
2023-03-26 11:34:41 +0530

    Sample Created

---
+++ EmployeeManagementSoftware/Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSoftware.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
